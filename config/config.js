d845873480e3d808bdb56c4f0984f55e3d66651a
Kaplia N <kapelkanadia@gmail.com>
2020-10-22 20:31:08 +0300

    base structure

---
+++ config/config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const rootPath = path.normalize(__dirname + '/..');
+const env = process.env.NODE_ENV || 'development';
+
+const config = {
+  development: {
+    root: rootPath,
+    app: {
+      name: 'market-test'
+    },
+    port: process.env.PORT || 3000,
+    db: 'postgres://postgres:test@localhost/market-test-development'
+  },
+
+  test: {
+    root: rootPath,
+    app: {
+      name: 'market-test'
+    },
+    port: process.env.PORT || 3000,
+    db: 'postgres://localhost/market-test-test'
+  },
+
+  production: {
+    root: rootPath,
+    app: {
+      name: 'market-test'
+    },
+    port: process.env.PORT || 3000,
+    db: 'postgres://localhost/market-test-production'
+  }
+};
+
+module.exports = config[env];
---
+++ src/controllers/index.js
@@ -0,0 +1,8 @@
+
+function index (req, res, next) {
+    res.sendStatus(200);
+}
+
+module.exports = {
+    index: index
+};
